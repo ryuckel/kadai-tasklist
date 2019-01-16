997d42637e84e35df1a2fa4d28b598662bd92845
EC2 Default User <ec2-user@ip-172-31-70-32.ec2.internal>
2019-01-16 14:03:15 +0000

    taskform

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
       @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
@@ -12,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
       if @task.save
           
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,28 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # get 'users/index'
 
-  get 'sessions/create'
+  # get 'users/show'
 
-  get 'sessions/destroy'
+  # get 'users/new'
+
+  # get 'users/create'
+
+  # get 'toppages/index'
+
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
+  resources :tasks
   resources :users, only: [:index, :show, :new, :create]
 end
